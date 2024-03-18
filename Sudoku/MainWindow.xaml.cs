31e7e04530f603660c96a4ea1dbfa74a1714c09c
Gr4v3d <122825815+Gr4v3d@users.noreply.github.com>
2024-03-18 01:31:18 +0100

    Część Logiki gry
    Jeszcze nie sprawne

--- Sudoku/MainWindow.xaml.cs
+++ Sudoku/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml.Linq;
@@ -36,7 +37,7 @@ namespace Sudoku
                     TextBox tb = new TextBox();
                     string name = $"tb{i}{j}";
                     tb.Name = name;
-                    tb.Text = (i*10+(j+1)).ToString();
+                    tb.Text = ((i*10+(j+1))%9).ToString();
                     root.Children.Add(tb);
                     RegisterName(tb.Name, tb);
                     Grid.SetRow(tb, i);
@@ -51,15 +52,92 @@ namespace Sudoku
             guzik.Click += new RoutedEventHandler(sprawdz);
             root.Children.Add(guzik);
             Grid.SetRow(guzik, 9);
-            Grid.SetColumn(guzik, 4);
+            Grid.SetColumn(guzik, 3);
+            Grid.SetColumnSpan(guzik, 3);
         }
 
         private void sprawdz(object sender, RoutedEventArgs e)
         {
-            przypisz();
+            bool Kontynuuj = przypisz();
+            if (Kontynuuj)
+            {
+                GameLogic();
+            }
         }
-        private void przypisz()
+        private void GameLogic()
         {
+            //Przeszukanie po kwadratach
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    List<int> Search = new List<int>();
+                    bool problem = false;
+                    for(int k =  0; k < 3; k++)
+                    {
+                        for(int l = 0; l < 3; l++)
+                        {
+                            if (k == 0 && l == 0)
+                            {
+                                Search.Add(numery[k+(3*i), l+(3*j)]);
+                                continue;
+                            }
+                            foreach (int item in Search)
+                            {
+                                if (item == numery[k + (3 * i), l + (3 * j)]) problem = true; break;
+                            }
+                            if (problem)
+                            {
+                                ErrorInABox(i,j);
+                                break;
+                            }
+                            Search.Add(numery[k + (3 * i), l + (3 * j)]);
+                        }
+                    }
+                }
+            }
+            //Przeszukanie po Rzędach
+            for (int i = 0; i < 9; i++)
+            {
+                List<int> Search = new List<int>(numery[i,0]);
+                bool problem = false;
+                for(int j = 1; j< 9; j++)
+                {
+                    foreach (int item in Search)
+                    {
+                        if(item == numery[i,j]) problem = true; break;
+                    }
+                    if (problem)
+                    {
+                        ErrorInARow(i);
+                        break;
+                    }
+                    Search.Add(numery[i,j]);
+                }
+            }
+            //Przeszukanie po kolumnach
+            for (int i = 0; i < 9; i++)
+            {
+                List<int> Search = new List<int>(numery[0, i]);
+                bool problem = false;
+                for (int j = 1; j < 9; j++)
+                {
+                    foreach (int item in Search)
+                    {
+                        if (item == numery[j, i]) problem = true; break;
+                    }
+                    if (problem)
+                    {
+                        ErrorInAColumn(j);
+                        break;
+                    }
+                    Search.Add(numery[j, i]);
+                }
+            }
+        }
+        private bool przypisz()
+        {
+            bool CzyJestOk = true;
             for (int i = 0; i < 9; i++)
             {
                 for (int j = 0; j < 9; j++)
@@ -75,7 +153,8 @@ namespace Sudoku
                         int temp = Convert.ToInt32(tb.Text.ToString());
                         if(temp <1 || temp > 10)
                         {
-                            tb.Background = Brushes.Yellow;
+                            tb.Background = Brushes.Red;
+                            CzyJestOk = false;
                         }
                         else
                         {
@@ -84,14 +163,50 @@ namespace Sudoku
                     }
                     catch
                     {
-                        tb.Background = Brushes.Red;
+                        tb.Background = Brushes.DarkRed;
                         object guzik = (Button)root.FindName("guzik");
                         Button button = guzik as Button;
                         button.Content = "Proszę podać liczbę całkowitą";
+                        CzyJestOk = false;
                     }
                 }
             }
+            return CzyJestOk;
 
+        }
+        //Zmienia Kolor rzędu lub kolumny (jeśli bool jest true) na podany kolor
+        private void ErrorInARow(int index)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                string name = $"tb{i}{index}";
+                object objekt = root.FindName(name);
+                TextBox tb = objekt as TextBox;
+                tb.Background = Brushes.Yellow;
+            }
+        }
+        private void ErrorInAColumn(int index)
+        {
+            for(int i = 0; i < 9; i++)
+            {
+                string name = $"tb{index}{i}";
+                object objekt = root.FindName(name);
+                TextBox tb = objekt as TextBox;
+                tb.Background = Brushes.Orange;
+            }
+        }
+        private void ErrorInABox(int height,int width)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    string name = $"tb{height*3+i}{width*3+j}";
+                    object objekt = root.FindName(name);
+                    TextBox tb = objekt as TextBox;
+                    tb.Background = Brushes.OrangeRed;
+                }
+            }
         }
 
         public MainWindow()
