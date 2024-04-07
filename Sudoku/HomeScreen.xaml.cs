45efe38441dbaaadb0a03a47aa4b3943b641b03e
Gr4v3d <122825815+Gr4v3d@users.noreply.github.com>
2024-04-07 14:43:42 +0200

    Koniec
    Poprawienie starego kodu
    Dodanie nowych metod służących do stworzenia wartości w polach sudoku tak aby było one potem rozwiązywalne
    Nowe okno domowe służące do wywołania gry sudoku

---
+++ Sudoku/HomeScreen.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sudoku
+{
+    /// <summary>
+    /// Logika interakcji dla klasy HomeScreen.xaml
+    /// </summary>
+    public partial class HomeScreen : Window
+    {
+
+        public HomeScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var input = (TextBox)FindName("Input");
+            try
+            {
+                var hints = Convert.ToInt32(input.Text);
+                if(hints < 0 || hints > 81) 
+                {
+                    var message = "Proszę podać wartość całkowitą z przedziału 0 a 81";
+                    SendMessage(message);
+                }
+                var Window = new MainWindow(hints);
+                Window.Show();
+                Close();
+            }
+            catch 
+            {
+                var message = "Proszę podać wartość całkowitą z przedziału 0 a 81";
+                SendMessage(message);
+            }
+        }
+        void SendMessage(string message)
+        {
+            var caption = "Błędna wprowadzona wartość";            
+            MessageBoxButton button = MessageBoxButton.OK;
+            MessageBoxImage image = MessageBoxImage.Error;
+            MessageBoxResult result;
+
+            result = MessageBox.Show(message, caption, button, image,MessageBoxResult.Yes);
+        }
+    }
+}
--- Sudoku/MainWindow.xaml.cs
+++ Sudoku/MainWindow.xaml.cs
@@ -1,193 +1,374 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.Common;
+using System.Diagnostics.Metrics;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Media.Media3D;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml.Linq;
 
-namespace Sudoku
+namespace Sudoku;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    protected override void OnClosed(EventArgs e)
     {
-        public int[,] numery = new int[9, 9];
-        public TextBox[,] okna = new TextBox[9, 9];
-        public void TworzGrid()
+        base.OnClosed(e);
+
+        Application.Current.Shutdown();
+    }
+    const int SizeOfGrid = 9;
+    int SRSize = (int)Math.Floor(Math.Sqrt(SizeOfGrid));
+    public int[,] Numbers = new int[9, 9];
+    public TextBox[,] TextBoxes = new TextBox[9, 9];
+    
+    void CreateMesh()
+    {
+        for (int i = 0; i < SizeOfGrid; i++)
         {
-            for (int i = 0; i < 9; i++)
+            for (int j = 0; j < SizeOfGrid; j++) //Zrób z tego metode
             {
-                root.ColumnDefinitions.Add(new ColumnDefinition());
+                TextBoxes[i, j] = new TextBox();
+                TextBoxes[i, j].IsEnabled = false;
+                root.Children.Add(TextBoxes[i, j]);
+                Grid.SetRow(TextBoxes[i, j], i);
+                Grid.SetColumn(TextBoxes[i, j], j);
             }
-            for (int i = 0; i < 9; i++)
+        }
+    }
+    void AddButton()
+    {
+        var button = new Button();
+        button.Content = "Sprawdź!";
+        button.Name = "Button1";
+        RegisterName(button.Name, button);
+        button.Click += new RoutedEventHandler(Game);
+        root.Children.Add(button);
+        Grid.SetRow(button, 9);
+        Grid.SetColumn(button, 3);
+        Grid.SetColumnSpan(button, 3);
+    }
+    public void CreateUI()
+    {
+        for (int i = 0; i < SizeOfGrid; i++)
+        {
+            root.ColumnDefinitions.Add(new ColumnDefinition());
+            root.RowDefinitions.Add(new RowDefinition());
+        }
+        root.RowDefinitions.Add(new RowDefinition());
+        CreateMesh();
+        AddButton();
+        
+    }
+
+    private void Game(object sender, RoutedEventArgs e)
+    {
+        ColourReset();
+        CheckInputsForErrors();
+        if (GameLogic())
+        {
+            switch (EndOfGame())
             {
-                //Przesuń do loopa wyżej
-                root.RowDefinitions.Add(new RowDefinition());
-                for (int j = 0; j < 9; j++) //Zrób z tego metode
+                case MessageBoxResult.Yes:
+                    var start = new HomeScreen();
+                    start.Show();
+                    this.Close();
+                    break;
+                case MessageBoxResult.No:
+                    System.Windows.Application.Current.Shutdown();
+                    break;
+            }
+        }
+    }
+    MessageBoxResult EndOfGame()
+    {
+        var message = "Udało ci się, wygrałeś!\n Czy chcesz zagrać ponownie ?";
+        var caption = "Koniec Gry";
+        var image = MessageBoxImage.None;
+        return MessageBox.Show(message, caption,MessageBoxButton.YesNo);
+    }
+    private bool GameLogic()
+    {
+        
+        var win = true;
+        //Jednoczesne przeszukanie po rzędach i kolumnach
+        for (int i = 0; i < SizeOfGrid; i++)
+        {
+            var rowState = CheckRow(i);
+            if (rowState == 0)
+            {
+                win = false;
+                ColourRow(i);
+            }
+            else if (rowState == 1) win = false;
+
+            var colState = CheckColumn(i);
+            if (colState == 0)
+            {
+                win = false;
+                ColourColumn(i);
+            }
+            else if (colState == 1) win = false;
+        }
+        //Przeszukanie wewnątrz kwadratów 3x3
+        for(int i = 0; i < SizeOfGrid; i+= SRSize)
+        {
+            for(int j = 0;j<SizeOfGrid; j+= SRSize)
+            {
+                if (!CheckInBox(i, j))
                 {
-                    TextBox tb = new TextBox();
-                    string name = $"tb{i}{j}";
-                    tb.Name = name;
-                    tb.Text = ((i*10+(j+1))%9).ToString();
-                    root.Children.Add(tb);
-                    RegisterName(tb.Name, tb);
-                    Grid.SetRow(tb, i);
-                    Grid.SetColumn(tb, j);
-                    okna[i,j] = tb;
+                    ColourBox(i, j); 
+                    win = false;
                 }
             }
-            root.RowDefinitions.Add(new RowDefinition());
-            Button guzik = new Button();
-            guzik.Content = "Guzior";
-            guzik.Name = "guzik";
-            RegisterName(guzik.Name,guzik);
-            guzik.Click += new RoutedEventHandler(sprawdz);
-            root.Children.Add(guzik);
-            Grid.SetRow(guzik, 9);
-            Grid.SetColumn(guzik, 3);
-            Grid.SetColumnSpan(guzik, 3);
+        }
+        return win;
+    }
+    //Sprawdza czy istnieją powtórzenia wewnątrz kwadratów
+    //Zmienne Row i Column wyznaczają kwadrat który ma być przeszukany
+    //na przykład Row = 1 i Column = 1 oznacza centralny kwadrat Sudoku, a Row = 0 i Column = 0, górny lewy kwadrat.
+    bool CheckInBox(int Row, int Column)
+    {
+        var box = new List<int>();
+        for(int i = 0;i < SRSize; i++)
+        {
+            for(int j = 0; j < SRSize; j++)
+            {
+                if (Numbers[Row + i,Column + j] != 0)
+                {
+                    if (box.Contains(Numbers[Row + i, Column + j]))
+                    {                        
+                        return false;
+                    }
+                    box.Add(Numbers[Row + i, Column + j]);
+                }
+            }
+        }
+        return true;
+    }
+    int CheckRow(int Row)
+    {
+        //Zmienna lokalna po to aby zdecydować czy w rzędzie występują powtórzenia, czy występują jakiekolwiek zera (miejsca puste/błedne) czy wszystko jest w porządku
+        //2 - Wszystko jest w porządku
+        //1 - występują zera
+        //0 - powtórzenie
+        var state = 2;
+        var knownElements = new List<int>();
+        for (int j = 0; j < SizeOfGrid; j++)
+        {
+            //0 jest domyślną wartością dla INT
+            //Więc jeśli nie ma 0 oznacza to że gracz zmienił wartość w oknie
+            if (Numbers[Row, j] != 0)
+            {
+                //Jeśli w liście znajduje się już element, podkreślam cały rząd
+                //tak samo działa przeszukanie po kolumnach
+                if (knownElements.Contains(Numbers[Row, j]))
+                {
+                    return 0;
+                }
+                knownElements.Add(Numbers[Row, j]);
+            }
+            else state = 1;
+        }
+        return state;
+    }
+    //Działa tak samo jak CheckRow
+    int CheckColumn(int Column)
+    {
+        var state = 2;
+        var knownElements = new List<int>();
+        for (int j = 0; j < SizeOfGrid; j++)
+        {
+            if (Numbers[j, Column] != 0)
+            {
+                if (knownElements.Contains(Numbers[j, Column]))
+                {
+                    return 0;
+                }
+                knownElements.Add(Numbers[j, Column]);
+            }
+            else state = 1;
+        }
+        return state;
+    }
+    void ColourBox(int Row, int Column)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                var rowIndex = Row  + i;
+                var colIndex = Column  + j;
+                TextBoxes[rowIndex, colIndex].Background = Brushes.Yellow;
+            }
+        }
+    }
+    void ColourRow(int Row)
+    {
+        for(int i = 0; i<SizeOfGrid; i++)
+        {
+            TextBoxes[Row,i].Background = Brushes.Gold;
+        }
+    }
+    void ColourColumn(int Column)
+    {
+        for (int i = 0; i < SizeOfGrid; i++)
+        {
+            TextBoxes[i,Column].Background = Brushes.Orange;
         }
 
-        private void sprawdz(object sender, RoutedEventArgs e)
+    }
+
+    void ColourReset()
+    {
+        for(int i = 0; i<SizeOfGrid; i++)
         {
-            bool Kontynuuj = przypisz();
-            if (Kontynuuj)
+            for(int j = 0; j<SizeOfGrid; j++)
             {
-                GameLogic();
+                TextBoxes[i, j].Background = Brushes.White;
             }
         }
-        private void GameLogic()
+    }
+    private void CheckInputsForErrors()
+    {
+        for (int i = 0; i < SizeOfGrid; i++)
         {
-            //Przeszukanie po kwadratach
-            for(int i = 0; i < 3; i++)
+            for (int j = 0; j < SizeOfGrid; j++)
             {
-                for(int j = 0; j < 3; j++)
+                if (TextBoxes[i, j].Text != "")
                 {
-                    ErrorInABox(i, j);
-                }
-            }
-            //Przeszukanie po Rzędach
-            for (int i = 0; i < 9; i++)
-            {
-                List<int> Search = new List<int>(numery[i,0]);
-                bool problem = false;
-                for(int j = 1; j< 9; j++)
-                {
-                    foreach (int item in Search)
-                    {
-                        if(item == numery[i,j]) problem = true; break;
-                    }
-                    if (problem)
-                    {
-                        ErrorInARow(i);
-                        break;
-                    }
-                    Search.Add(numery[i,j]);
-                }
-            }
-            //Przeszukanie po kolumnach
-            for (int i = 0; i < 9; i++)
-            {
-                List<int> Search = new List<int>(numery[0, i]);
-                bool problem = false;
-                for (int j = 1; j < 9; j++)
-                {
-                    foreach (int item in Search)
-                    {
-                        if (item == numery[j, i]) problem = true; break;
-                    }
-                    if (problem)
-                    {
-                        ErrorInAColumn(j);
-                        break;
-                    }
-                    Search.Add(numery[j, i]);
-                }
-            }
-        }
-        private bool przypisz()
-        {
-            bool CzyJestOk = true;
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    /*string name = "tb";
-                    name = name + i;
-                    name = name + j;
-                    object objekt = root.FindName(name);
-                    TextBox tb = objekt as TextBox;
-                    tb.Background = null;*/
                     try
                     {
-                        int temp = Convert.ToInt32(okna[i,j].Text);
-                        if(temp <1 || temp > 10)
+                        var temp = Convert.ToInt32(TextBoxes[i, j].Text);
+                        if (temp < 1 || temp > 9)
                         {
-                            okna[i,j].Background = Brushes.Red;
-                            CzyJestOk = false;
+                            TextBoxes[i, j].Background = Brushes.Blue;
+                            Numbers[i, j] = 0; // Jeśli podany numer nie jest z przedziału 1 a 9 przypisujemy wartość domyślną 0
+                            continue;
                         }
+                        Numbers[i, j] = temp;
                     }
                     catch
                     {
-                        okna[i,j].Background = Brushes.DarkRed;
-                        object guzik = (Button)root.FindName("guzik");
-                        Button button = guzik as Button;
-                        button.Content = "Proszę podać liczbę całkowitą";
-                        CzyJestOk = false;
+                        TextBoxes[i, j].Background = Brushes.Purple;
+                        Numbers[i, j] = 0; // Jeśli w textboxie nie wpisanej liczby całkowitej, przypisz wartość domyślną 0
                     }
                 }
             }
-            return CzyJestOk;
+        }
+    }
+   
 
-        }
-        //Zmienia Kolor rzędu lub kolumny (jeśli bool jest true) na podany kolor
-        private void ErrorInARow(int index)
+    public MainWindow(int Hints)
+    {
+        new HomeScreen();
+        InitializeComponent();
+        CreateUI();
+        SetUpGame(Hints);
+    }
+
+    public void SetUpGame(int HowManyHints)
+    {
+        var rng = new Random();
+        for (int i = 0; i < SizeOfGrid; i += SRSize)
         {
-            for (int i = 0; i < 9; i++)
+                FillBox(i,i);                            
+        }
+        FillCells(0, SRSize);
+        for(int i = 0;i < SizeOfGrid; i++)
+        {
+            for(int j = 0; j < SizeOfGrid; j++)
             {
-                //okna[index,i].Background = Brushes.Yellow;
+                TextBoxes[i,j].Text = Numbers[i,j].ToString();
             }
         }
-        private void ErrorInAColumn(int index)
+        RemoveElements((SizeOfGrid*SizeOfGrid)- HowManyHints);
+    }
+    void RemoveElements(int AmountToRemove)
+    {
+        while(AmountToRemove > 0)
         {
-            for(int i = 0; i < 9; i++)
-            {
-                //okna[i, index].Background = Brushes.Orange;
-            }
+            var rng = new Random();
+            var x = rng.Next(SizeOfGrid);
+            var y = rng.Next(SizeOfGrid);
+            if (Numbers[x,y] == 0) continue;
+            Numbers[x, y] = 0;
+            TextBoxes[x, y].Text = "";
+            TextBoxes[x, y].IsEnabled = true;
+            AmountToRemove--;
         }
-        // Wysokość i szerokość musi być od 0 - 2 żeby działało zapamiętaj
-        private void ErrorInABox(int height,int width)
+    }
+    public void FillBox(int RowStart, int ColStart)
+    {
+        var rng = new Random();
+        var elements = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        for (int i = 0; i < SRSize; i++)
         {
-            List<string> znaneWartosci = new List<string>();
-            for (int i = 0; i < 3; i++)
+            for (int j = 0; j < SRSize; j++)
             {
-                for(int j = 0; j < 3; j++)
+                while (true)
                 {
-                    int row = i + (height * 3);
-                    int column = j + (width * 3);
-                    foreach (string item in znaneWartosci)
+                    var index = rng.Next(elements.Count);
+                    Numbers[RowStart + i, ColStart + j] = elements[index];
+                    if (CheckInBox(RowStart, ColStart))
                     {
-                        if(item == okna[row, column].Text) okna[row,column].Background = Brushes.DarkOrange;
+                        elements.RemoveAt(index);
+                        TextBoxes[RowStart + i, ColStart + j].Text = Numbers[RowStart + i, ColStart + j].ToString();
+                        break;
                     }
                 }
             }
         }
-
-        public MainWindow()
+    }
+    public bool FillCells(int Row, int Column)
+    {
+        if ( Column >= SizeOfGrid && Row < SizeOfGrid - 1)
         {
-            InitializeComponent();
-            TworzGrid();
+            Row = Row + 1;
+            Column = 0;
         }
+        if(Row >= SizeOfGrid && Column >= SizeOfGrid)
+        {
+            return true;
+        }
+        if(Row < SRSize)
+        {
+            if(Column < SRSize)
+                Column = SRSize;
+        }
+        else if(Row < SizeOfGrid - SRSize)
+        {
+            if(Column == (int)(Row/SRSize)*SRSize)
+                Column = Column + SRSize;
+        }
+        else
+        {
+            if (Column == SizeOfGrid - SRSize)
+            {
+                Row = Row + 1;
+                Column = 0;
+                if (Row >= SizeOfGrid)
+                    return true;
+            }
+        }
+        for(int num = 1; num <=SizeOfGrid ;num++)
+        {
+            Numbers[Row, Column] = num;
+            if (CheckAll(Row, Column))
+            {
+                if (FillCells(Row, Column + 1))
+                    return true;
+                
+            }
+            Numbers[Row, Column] = 0;
+        }
+        return false;
+    }
+    bool CheckAll(int Row, int Column)
+    {
+        return (CheckRow(Row) != 0 && CheckColumn(Column) != 0 && CheckInBox(Row - Row % SRSize, Column - Column % SRSize));
     }
 }
